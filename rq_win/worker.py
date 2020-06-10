7a4d8bacf614624ddd4532e55dee7c5325d4121b
David Murray <dmurray@exo.quebec>
2020-06-10 12:51:14 -0400

    Make sure the job gets added to the started job registry.

--- rq_win/worker.py
+++ rq_win/worker.py
@@ -72,10 +72,12 @@ class WindowsWorker(rq.Worker):
 
         self._is_horse = False
 
-    def perform_job(self, job, queue):
+    def perform_job(self, job, queue, heartbeat_ttl=None):
         """Performs the actual work of a job.  Will/should only be called
         inside the work horse's process.
         """
+        self.prepare_job_execution(job, heartbeat_ttl)
+
         self.procline('Processing %s from %s since %s' % (
             job.func_name,
             job.origin, time.time()))
